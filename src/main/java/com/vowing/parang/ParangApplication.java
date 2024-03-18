790ead6e7c84ee917c7f5eff42c0b8767da00a23
jominuk <jominuk1025@naver.com>
2024-03-19 08:39:49 +0900

    first commit

---
+++ src/main/java/com/vowing/parang/ParangApplication.java
@@ -0,0 +1,13 @@
+package com.vowing.parang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParangApplication.class, args);
+    }
+
+}
