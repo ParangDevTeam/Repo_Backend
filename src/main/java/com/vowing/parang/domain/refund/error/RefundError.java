2bfce875cffd73eb393b9bb2cb7adcebfe94cc7e
jominuk <jominuk1025@naver.com>
2024-03-27 12:24:24 +0900

    refund error 처리

--- src/main/java/com/vowing/parang/domain/refund/error/RefundError.java
+++ src/main/java/com/vowing/parang/domain/refund/error/RefundError.java
@@ -1,4 +1,8 @@
 package com.vowing.parang.domain.refund.error;
 
-public class RefundError {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class RefundError extends RuntimeException {
 }
